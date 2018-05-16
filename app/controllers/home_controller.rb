acbf6f4297d4da7a871f25dcd8652c3079ec81cf
EC2 Default User <ec2-user@ip-172-31-1-179.us-east-2.compute.internal>
2018-05-16 13:11:15 +0000

    wow

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def fir
+  end
+  
+  def sec
+  end
+  
+  def thi
+  end
+  
+  def forth
+  end
+  
+  def fif
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/fir'
+  get 'home/sec'
+  get 'home/thi'
+  get 'home/forth'
+  get 'home/fif'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
